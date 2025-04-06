86a64553fefe005cf61421e0fa52d0f3c866f4bf
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2025-04-06 21:33:03 +0300

    feat: implement signal logic to delete building instances with no profiles upon deletion of a profile instance

--- buildings/apps.py
+++ buildings/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class BuildingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'buildings'
+
+    def ready(self):
+        import buildings.signals
---
+++ buildings/signals.py
@@ -0,0 +1,15 @@
+from django.db.models.signals import pre_delete, post_delete
+from django.dispatch import receiver
+from users.models import Profile
+from buildings.models import Building
+
+@receiver(pre_delete, sender=Profile)
+def save_related_buildings_before_profile_delete(sender, instance, **kwargs):
+    instance._related_buildings = list(instance.buildings.all())
+
+@receiver(post_delete, sender=Profile)
+def delete_orphaned_buildings_after_profile_delete(sender, instance, **kwargs):
+    print(instance._related_buildings)
+    for building in instance._related_buildings:
+        if not building.profile.all().exists():
+            building.delete()
